6cd60e4bdac0e7865701b0403a36c52b74922799
Gleb Sidora <gleb.sidora@gmail.com>
2026-03-18 21:21:08 +0100

    typo

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -315,6 +315,7 @@ if [ "$(id -u)" = "0" ]; then
   chown -R "$CODOS_USER:$CODOS_USER" "$CODOS_HOME/codos"
 
   info "Re-running as '$CODOS_USER'..."
+  cd "$CODOS_HOME"
   exec sudo -u "$CODOS_USER" -H bash "$CODOS_HOME/codos/scripts/bootstrap.sh" "$@"
 fi
 
